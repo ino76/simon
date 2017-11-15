8b6d05a838f184f9499c6557335ec94ab8476067
ino <ino@localhost.localdomain>
2017-11-15 19:28:14 +0100

    rozpracovano ...

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// Simon says
+// Simon game
 // author: Daniel Cech 2017
+
+
+
+
+
+
+function playSound(color) {
+    const audio = document.getElementById(color);
+    audio.play()
+    light(color)
+}
+
+function light(color) {
+    const element = document.querySelector('data[' + color + ']')
+}
