c6277dbb2f0dadf7603d36c22e1e9e0bb4fe384c
Daniel Cech <daniel8cech@gmail.com>
2017-11-16 20:59:25 +0100

    hotovo :D

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 // author: Daniel Cech 2017
 
 
+const startButton = document.getElementById('start')
+const result = document.getElementById('result')
 const colors = ["hsl(16, 83%, 43%)", "hsl(153, 72%, 45%)", "hsl(204, 78%, 40%)", "hsl(60, 78%, 49%)"]
 const buttons = document.querySelectorAll('.simBut')
 const wrongSound = document.getElementById('wrong')
+const wrongSound2 = document.getElementById('wrong2')
 setColors()
 setListeners()
 
-let sequence = [0,1,3,3,2,3,0,1]
+let sequence = []
 let active = false
 let interval
+let position = 0
+let points = 0
+let games = 1
+
+
+function startBle(value) {
+    startButton.disabled = value
+}
 
 
 function start() {
@@ -31,7 +42,26 @@ function setListeners() {
 }
 
 
-function listen() {
+function listen(pushedButton) {
+    let sequenceLength = sequence.length
+    console.log("ma zmacknout: " + sequence[position] + ", zmackl: " + pushedButton)
+
+    if (sequence[position] === pushedButton) {
+        position++
+        playSound(pushedButton)
+        lightUp(pushedButton)
+        
+        if (position === sequenceLength) {
+            position = 0
+            points++
+            start()
+        }
+
+    } else {
+        exit()
+    }
+
+
 }
 
 
@@ -52,6 +82,7 @@ function playSequence() {
 
 function playSound(color) {
     const audio = document.getElementById(color);
+    audio.currentTime = 0;
     audio.play()
 }
 
@@ -82,18 +113,43 @@ function push(color) {
 function playerPush(color) {
     // const thisButton = document.querySelector(`div[data-color="${color}"`)
     if (active === true) {
-        wrongSound.play()
-        clearInterval(interval)
-        active = false
+        exit()
         return
     }
-    playSound(color)
-    lightUp(color)
+    if (sequence.length > 0) {
+        listen(Number(color))
+        return
+    }
 }
 
+
+function setNew() {
+    position = 0
+    sequence = []
+    result.textContent = ""
+    points = 0
+    startBle(true)
+    start()
+}
+
+function exit() {
+    
+    if (games != 4){
+        wrongSound.play()
+    } else {
+        wrongSound2.play()
+    }
+    clearInterval(interval)
+    active = false
+    result.textContent = 'You have ' + points + ' points.'
+    games++
+    startBle(false)
+}
 
 function addNext() {
     let random = Math.floor(Math.random() * 4)
     sequence.push(random)
+    // TODO vymazat log
     console.log(sequence)
+    left = sequence.length
 }
